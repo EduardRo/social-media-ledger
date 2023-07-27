e63c595147d2ac5c4c92550343a9245167ebbaf2
EduardRo <eduardstancu@yahoo.com>
2023-07-27 20:56:41 +0300

    creations created

---
+++ app/Http/Controllers/CreationController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Creation;
+use App\Http\Requests\StoreCreationRequest;
+use App\Http\Requests\UpdateCreationRequest;
+
+class CreationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCreationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Creation $creation)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Creation $creation)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCreationRequest $request, Creation $creation)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Creation $creation)
+    {
+        //
+    }
+}

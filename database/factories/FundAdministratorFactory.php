f3d36d02d0b211bb7184fda8d8efa1defcb6e460
EduardRo <eduardstancu@yahoo.com>
2023-08-10 10:26:16 +0300

    fundAdministrator

--- database/factories/FundAdministratorFactory.php
+++ database/factories/FundAdministratorFactory.php
@@ -17,7 +17,20 @@ class FundAdministratorFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->company,
+            'country' => $this->faker->country,
+            'city' => $this->faker->city,
+            'address' => $this->faker->address(),
+            'contact_person' => $this->faker->name(),
+            'phone' => $this->faker->phoneNumber(),
+            'email' => $this->faker->email,
+            'administrator_name' => $this->faker->firstName,
+            'administrator_surname' => $this->faker->lastName,
+            'administrator_phone' => $this->faker->phoneNumber(),
+            'administrator_email' => $this->faker->email()
+
+
+
         ];
     }
 }
--- database/seeders/FundAdministratorSeeder.php
+++ database/seeders/FundAdministratorSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\FundAdministrator;
+// use App\Models\Fund;
 
 class FundAdministratorSeeder extends Seeder
 {
@@ -12,6 +14,6 @@ class FundAdministratorSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        FundAdministrator::factory()->count(10)->create();
     }
 }
